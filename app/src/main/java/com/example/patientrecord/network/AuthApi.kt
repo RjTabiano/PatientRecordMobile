bd05fdbb8829c6afcff127fbc18c069a33ea04cc
RjTabiano <tabianorj@gmail.com>
2024-03-05 21:41:18 +0800

    Update

---
+++ app/src/main/java/com/example/patientrecord/network/AuthApi.kt
@@ -0,0 +1,16 @@
+package com.example.patientrecord.network
+
+import android.provider.ContactsContract.CommonDataKinds.Email
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
+interface AuthApi {
+
+    @FormUrlEncoded
+    @POST("auth/login")
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Any
+}
