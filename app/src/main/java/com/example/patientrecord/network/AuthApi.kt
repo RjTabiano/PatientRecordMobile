7cf2eb48b27b7de533b7b1a746ffbdd7ec2ee39d
RjTabiano <tabianorj@gmail.com>
2024-03-06 00:05:39 +0800

    update

--- app/src/main/java/com/example/patientrecord/network/AuthApi.kt
+++ app/src/main/java/com/example/patientrecord/network/AuthApi.kt
@@ -1,6 +1,7 @@
 package com.example.patientrecord.network
 
 import android.provider.ContactsContract.CommonDataKinds.Email
+import com.example.patientrecord.responses.LoginResponse
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
@@ -9,8 +10,8 @@ interface AuthApi {
 
     @FormUrlEncoded
     @POST("auth/login")
-    fun login(
+    suspend fun login(
         @Field("email") email: String,
         @Field("password") password: String
-    ): Any
+    ): LoginResponse
 }
---
+++ app/src/main/java/com/example/patientrecord/responses/LoginResponse.kt
@@ -0,0 +1,5 @@
+package com.example.patientrecord.responses
+
+data class LoginResponse(
+    val user: User
+)
