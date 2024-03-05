8192a73f3a597149465ba57dbe51bc493a2a89a5
RjTabiano <tabianorj@gmail.com>
2024-03-05 20:50:46 +0800

    update

--- app/src/main/java/com/example/patientrecord/auth/AuthActivity.kt
+++ app/src/main/java/com/example/patientrecord/auth/AuthActivity.kt
@@ -1,24 +1,21 @@
-package com.example.patientrecord
+package com.example.patientrecord.auth
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.patientrecord.auth.AuthActivity
+import com.example.patientrecord.R
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_auth)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        finish()
-        startActivity(Intent(this, AuthActivity::class.java))
     }
 }
