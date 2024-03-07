9e152c39ac2665d3a00c4c1d88a888bc362db969
RjTabiano <tabianorj@gmail.com>
2024-03-07 22:19:45 +0800

    Update RegisterActivity.kt

--- app/src/main/java/com/example/patientrecord/ui/auth/RegisterActivity.kt
+++ app/src/main/java/com/example/patientrecord/ui/auth/RegisterActivity.kt
@@ -1,13 +1,24 @@
 package com.example.patientrecord.ui.auth
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.patientrecord.R
+import com.example.patientrecord.controller.AuthController
+import com.example.patientrecord.model.User
+import com.example.patientrecord.network.ApiCallback
+import com.example.patientrecord.network.ApiResponse
+import com.example.patientrecord.ui.UserView.UserHomeActivity
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val authController = AuthController()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,6 +27,30 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val buttonRegister = findViewById<Button>(R.id.buttonRegister)
+        val userNameField = findViewById<EditText>(R.id.userNameField)
+        val userEmailField = findViewById<EditText>(R.id.userEmailField)
+        val passwordField = findViewById<EditText>(R.id.passwordField)
+
+        buttonRegister.setOnClickListener {
+            val name = userNameField.text.toString()
+            val email = userEmailField.text.toString()
+            val password = passwordField.text.toString()
+
+            authController.registerUser(name, email, password, object : ApiCallback<User> {
+                override fun onSuccess(response: User) {
+                    Toast.makeText(this@LoginActivity, "Register successful!", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this@LoginActivity, UserHomeActivity::class.java)
+                    startActivity(intent)
+                    finish() // Optional: Close the current activity if necessary
+                }
+
+                override fun onFailure(error: ApiResponse) {
+                    Toast.makeText(this@LoginActivity, "Register failed: $error", Toast.LENGTH_SHORT).show()
+                }
+            })
         }
     }
 }
